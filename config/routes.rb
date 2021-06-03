c4886bb624adb17fe5c450eeb7d1b0abec4dc850
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-06-03 12:20:18 +0000

    [start]task4

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+   devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
+
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+
+  resources :books do
+  	resource :favorites, only: [:create, :destroy]
+  	resources :book_comments, only: [:create, :destroy]
+  end
+
+end
